9c75a65010337b2351b0e8a1febfa30df877499e
kagerka <kagerka@gmail.com>
2021-08-14 12:48:20 +0300

    fix: add cards and form

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './Form/Form';
+import Card from './Card/Card';
+import style from './App.module.scss';
 
-function App() {
+const App = () => {
+  const [formValues, setFormValues] = useState([]);
   return (
-    <React.StrictMode>
-      <div></div>
-    </React.StrictMode>
+    <div>
+      <Form setFormValues={setFormValues} />
+      <main className={style.main}>
+        {formValues.map((item, idx) => {
+          return <Card item={item} key={idx} />;
+        })}
+      </main>
+    </div>
   );
-}
+};
 
 export default App;
