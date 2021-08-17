24489f1809f6b4dbb01a65d8324682fbc566fb74
kagerka <kagerka@gmail.com>
2021-08-17 16:28:40 +0300

    fix: add constants

--- react-api/src/constants/constants.js
+++ react-api/src/constants/constants.js
@@ -1,1 +1,7 @@
+export const API_KEY = '6f95d39578f9eec6674ccbd1cdf823f6';
+// export const API_KEY = '618fa5190c15432a8170aa42844cb4f9'; newsapi.org
+export const API_SECRET_KEY = '60496c7e68b9e0e6';
 export const ZERO = 0;
+export const ONE = 1;
+export const TEN = 10;
+export const ONE_THOUSAND = 1000;
