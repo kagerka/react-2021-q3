2108655c15118299dc50452272c9f96c5ade7477
kagerka <kagerka@gmail.com>
2021-08-18 15:46:12 +0300

    refactor: remove unnecessary code

--- react-api/src/constants/constants.js
+++ react-api/src/constants/constants.js
@@ -1,5 +1,4 @@
 export const API_KEY = '6f95d39578f9eec6674ccbd1cdf823f6';
-// export const API_KEY = '618fa5190c15432a8170aa42844cb4f9'; newsapi.org
 export const API_SECRET_KEY = '60496c7e68b9e0e6';
 export const ZERO = 0;
 export const ONE = 1;
