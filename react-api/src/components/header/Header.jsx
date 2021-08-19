0a22b6cd7cffe0521a5b41539372e261214000bf
kagerka <kagerka@gmail.com>
2021-08-19 23:27:09 +0300

    init: add header component

---
+++ react-api/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <NavLink to='/' exact className={styles.link} activeClassName={styles.active_link}>
+          Home
+        </NavLink>
+        <NavLink to='/about' className={styles.link} activeClassName={styles.active_link}>
+          About
+        </NavLink>
+      </nav>
+      <div className={styles.title}>React Routing</div>
+    </header>
+  );
+};
+
+export default Header;
