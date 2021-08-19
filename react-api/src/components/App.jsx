db06a692b5d65400d0e976471b1683a1e01378bc
kagerka <kagerka@gmail.com>
2021-08-19 23:25:06 +0300

    feat: add routing for navigation

--- react-api/src/components/App.jsx
+++ react-api/src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './search/Search';
+import Header from './header/Header';
+import About from './about/About';
+import './App.module.scss';
+import NotFound from './error/NotFound';
 
 const App = () => {
   return (
-    <div>
-      <Search />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Search} />
+        <Route exact path='/about' component={About} />
+        <Route component={NotFound} />
+        {/* <Route path='/404' component={NotFound} /> */}
+        {/* <Redirect to='/404' /> */}
+      </Switch>
+    </BrowserRouter>
   );
 };
 
