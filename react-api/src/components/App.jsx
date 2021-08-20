6fe87b69460ec84c6daaa74a71cf60c95f85ee36
kagerka <kagerka@gmail.com>
2021-08-21 01:31:46 +0300

    feat: add details route

--- react-api/src/components/App.jsx
+++ react-api/src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Search from './search/Search';
 import Header from './header/Header';
 import About from './about/About';
 import './App.module.scss';
 import NotFound from './error/NotFound';
+import Details from './details/Details';
 
 const App = () => {
   return (
@@ -13,9 +15,8 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Search} />
         <Route exact path='/about' component={About} />
+        <Route exact path='/details/:id' render={(props) => <Details {...props} />} />
         <Route component={NotFound} />
-        {/* <Route path='/404' component={NotFound} /> */}
-        {/* <Redirect to='/404' /> */}
       </Switch>
     </BrowserRouter>
   );
