3b9f6398f9529a98e143d6dbdb5a6c68852107e5
kagerka <kagerka@gmail.com>
2021-08-21 01:30:30 +0300

    feat: add animation

--- react-api/src/components/about/About.jsx
+++ react-api/src/components/about/About.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 import styles from './About.module.scss';
 
 const About = () => {
+  const props = useSpring({
+    to: { opacity: 1, transform: 'translate3d(0,0,0)' },
+    from: { opacity: 0, transform: 'translate3d(-100%,0,0)' },
+  });
   return (
-    <div className={styles.about_wrapper}>
+    <animated.div style={props} className={styles.about_wrapper}>
       <img src='/images/about.svg' alt='' className={styles.main_image} />
-    </div>
+    </animated.div>
   );
 };
 
