4cf692b3b3c6ea7de323d1c676036df08f401fbe
kagerka <kagerka@gmail.com>
2021-08-21 01:32:32 +0300

    fix: add new constants

--- react-api/src/constants/constants.js
+++ react-api/src/constants/constants.js
@@ -3,4 +3,7 @@ export const API_SECRET_KEY = '60496c7e68b9e0e6';
 export const ZERO = 0;
 export const ONE = 1;
 export const TEN = 10;
+export const ELEVEN = 11;
+export const NINETEEN = 19;
+
 export const ONE_THOUSAND = 1000;
