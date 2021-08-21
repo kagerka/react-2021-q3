5a57f07d1c50536a31e07f6d921d0b3f0ef9b937
kagerka <kagerka@gmail.com>
2021-08-21 09:38:26 +0300

    fix: change search info

--- react-api/src/components/search/Search.jsx
+++ react-api/src/components/search/Search.jsx
@@ -95,19 +95,19 @@ function Search() {
           setPerPage={setPerPage}
         />
       ) : (
-        <NotFound searchValue={searchValue} beforeSearch={beforeSearch} />
-      )}
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className={styles.search_info}>
-          <img src='./icons/bot.svg' alt='' />
-          <p>
-            Type the text that you are looking for in the search field and click button "Search". It will automatically
-            show you results of the images.
-          </p>
+        <div>
+          <NotFound searchValue={searchValue} beforeSearch={beforeSearch} />
+          <div className={styles.search_info}>
+            <img src='./icons/bot.svg' alt='' />
+            <p>
+              Type the text that you are looking for in the search field and click button &quot;Search&quot;. It will
+              automatically show you results of the images.
+            </p>
+          </div>
         </div>
       )}
+
+      {isLoading ? <Loading /> : ''}
     </animated.div>
   );
 }
