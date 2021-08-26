ade987826f5d0cf5ccc39a2e9fff6fbb4e363ce3
kagerka <kagerka@gmail.com>
2021-08-26 03:41:05 +0300

    refactor: refactor code

--- react-api/src/services/getImage.js
+++ react-api/src/services/getImage.js
@@ -3,13 +3,11 @@ import { API_KEY } from '../constants/constants';
 
 const getImage = axios.create({
   baseURL: 'https://www.flickr.com/services',
-  // timeout: 5000,
+  timeout: 5000,
   params: {
     nojsoncallback: 1,
     format: 'json',
     api_key: API_KEY,
-    // auth_token: '72157719716571657-daefb401a9878aa1',
-    // api_sig: '9d1eaac3535514471b08dac819f1628b',
   },
 });
 
